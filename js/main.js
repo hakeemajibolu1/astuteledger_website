794ce27ec0d28320a17f67b34c9634d18adb5562
Hakeem Ajibolu <aspayer@gmail.com>
2020-10-05 17:57:51 +0100

    added and styled the contact form and footer, also used Google Maps API to embed the location map using some JavaScript

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// Initialize and add the map
+function initMap() {
+    // Your location
+    const loc = { lat: 6.524379, lng: 3.3792063 };
+
+    // Centered map on location
+    const map = new google.maps.Map(document.querySelector('.map'), 
+    {
+        zoom: 14, 
+        center: loc
+    });
+
+    // The marker, positioned at location
+    const marker = new google.maps.Marker({ position: loc, map: map});
+}
