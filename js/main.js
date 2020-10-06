cf68b7a95650b6aa9971eafaaea07b556805210f
Hakeem Ajibolu <aspayer@gmail.com>
2020-10-06 18:43:28 +0100

    created a JavaScript function that utilizes jQuery for smooth scrolling between sections

--- js/main.js
+++ js/main.js
@@ -13,3 +13,29 @@ function initMap() {
     // The marker, positioned at location
     const marker = new google.maps.Marker({ position: loc, map: map});
 }
+
+// Sticky menu background fade while scrolling
+window.addEventListener('scroll', function() {
+    if (window.scrollY > 150) {
+        document.querySelector('#navbar').style.opacity = 0.8;
+    } else {
+        document.querySelector('#navbar').style.opacity = 1;
+    }
+});
+
+// Smooth Scroll Function. "hash" refers to the IDs
+$('#navbar a, .btn').on('click', function(event) {
+    if (this.hash !== '') {
+        event.preventDefault();
+        
+        const hash = this.hash;
+
+        $('html, body').animate(
+            {
+                // Scroll from the top and the "-100" is to show the section title
+                scrollTop: $(hash).offset().top - 100 
+            }, 
+            800 // Scroll speed in milliseconds
+        );
+    }
+});
